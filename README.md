d22bd1fe550734afbe4015897b40007744f78cee
swathi.paipalle <swathi.paipalle@gmail.com>
2016-03-29 16:16:36 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+<h1>Angular2 Tour Of Heroes</h1>
+
+<h4>Prerequisites</h4>
+npm Install
+
+<h4>To run</h4>
+>cd angular2-tour-of-heroes <br>
+>npm start
---
+++ app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component } from 'angular2/core';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
+
+import { HeroService } from './hero.service';
+import { DashboardComponent } from './dashboard.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app/app.html',
+  styleUrls: ['public/css/app.component.css'],
+  directives: [ROUTER_DIRECTIVES],
+  providers: [
+    ROUTER_PROVIDERS,
+    HeroService
+  ]
+})
+@RouteConfig([
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true
+  },
+  {
+    path: '/detail/:id',
+    name: 'HeroDetail',
+    component: HeroDetailComponent
+  },
+  {
+    path: '/heroes',
+    name: 'Heroes',
+    component: HeroesComponent
+  }
+])
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
